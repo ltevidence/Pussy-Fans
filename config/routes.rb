803cf646a71373bab8ca4eb736304b41fb970e7d
ltevidence <40716120+ltevidence@users.noreply.github.com>
2021-06-08 13:25:12 +0200

    profile page update

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
   resources :orders
   resources :carts
   resources :users
   resources :items
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
