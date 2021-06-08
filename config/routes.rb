49e3caa84062b4bab2aeda711485d4cda753046c
nicolas <nicopelletier39@gmail.com>
2021-06-08 17:46:39 +0200

    development: routes: reduces the number of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :orders
-  resources :carts
-  resources :users
-  resources :items
+  resources :orders, only: [:index, :show]
+  resources :carts, only: [:index]
+  resources :users, only: [:show]
+  resources :items, only: [:index, :show]
 
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
