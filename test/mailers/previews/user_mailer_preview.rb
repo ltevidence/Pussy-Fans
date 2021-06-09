1f2a7346cc5a06086dbb7e2a52ffc507e08e116a
feliciebernard <felicie.bernard@outlook.fr>
2021-06-09 19:01:32 +0200

    add confirmation text

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,10 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
-def welcome_email
+  def welcome_email
 
-  UserMailer.welcome_email(User.first)
-end
+    UserMailer.welcome_email(User.first)
+  end
 
   def confirmation_email
 
