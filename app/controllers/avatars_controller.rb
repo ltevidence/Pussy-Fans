ef75ea2948a4cc9adf4297d3fb9ff8de84a41078
ltevidence <40716120+ltevidence@users.noreply.github.com>
2021-06-09 10:10:34 +0200

    user auth

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,5 +1,5 @@
 class AvatarsController < ApplicationController
-  
+  before_action :authenticate_user!
   def create
     @user = User.find(params[:user_id])
     @user.avatar.attach(params[:avatar])
