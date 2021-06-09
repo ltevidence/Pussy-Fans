538f907bfd10aa030a8d3417f2547f0871f82249
nicolas <nicopelletier39@gmail.com>
2021-06-09 15:16:38 +0200

    development: link order create controller to item index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,22 @@ class ItemsController < ActionController::Base
   def index
     @items = Item.all
     @item = @items.sample
+
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart = @current_user.cart
+    if (@current_user)
+      @cart
+    end
+  end
+
+  def create
+    puts "\n\nITEM = #{@item} #{item} #{params[:item]}\n\n"
+    @cart = current_user.cart
+    @order = Order.new(item: item, cart:@cart)
+    render root_path(locals: { :object_created => @order, :success_message => "BRAVO JJJJJJJ" })
   end
 
   private 
